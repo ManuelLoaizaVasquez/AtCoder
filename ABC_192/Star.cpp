11287c0257c14bd48a87a80c4cc4a77dafac0588
ManuelLoaizaVasquez <manulova999@gmail.com>
2021-02-20 10:33:26 -0500

    trivial

---
+++ ABC_192/Star.cpp
@@ -0,0 +1,34 @@
+//https://atcoder.jp/contests/abc192/tasks/abc192_a
+#include <bits/stdc++.h>
+#include <unistd.h>
+#define FAST_IO ios::sync_with_stdio(0);cin.tie(NULL)
+#define all(v) (v).begin(),(v).end()
+using namespace std;
+
+typedef long long Long;
+typedef long double Double;
+typedef unsigned long long ULong;
+typedef pair<Long, Long> Pair;
+
+const int N = 1e6;
+const Long INF = 1e18;
+const Double EPS = 1e-9;
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+Long random(Long a, Long b) { return uniform_int_distribution<Long> (a, b) (rng); }
+
+void Solve(void) {
+  Long n;
+  cin >> n;
+  Long ans = 100 - n % 100;
+  cout << ans << '\n';
+}
+
+int main(void) {
+  FAST_IO;
+  int t = 1;
+  //cin >> t;
+  while (t--) Solve();
+  return 0;
+}
+
